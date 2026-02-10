a3a97b5b894deb2ececcdad56882949020b96cd6
Marjan Haghighat <MarjanHD1372@gmail.com>
2026-02-10 12:03:33 +0000

    add application services and PollService

---
+++ Votingsystem.frontend/VotingSystem.Application/Services/IPollService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using VotingSystem.Domain;
+using System.Collections.Generic;
+
+namespace VotingSystem.Application.Services
+{
+   
+}
+
---
+++ Votingsystem.frontend/VotingSystem.Application/Services/PollService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VotingSystem.Application.Services
+{
+    internal class PollService
+    {
+    }
+}
