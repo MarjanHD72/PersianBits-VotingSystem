f78d45469ab9be96f8a26eb7e72aa9ea1fdfcda5
Marjan Haghighat <MarjanHD1372@gmail.com>
2026-02-14 15:05:52 +0000

    add ILoggingService interface

---
+++ Votingsystem/VotingSystem.Application/Services/Logging/ILoggingService.cs
@@ -0,0 +1,9 @@
+﻿namespace VotingSystem.Application.Services.Logging
+{
+    public interface ILoggingService
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+    }
+}
