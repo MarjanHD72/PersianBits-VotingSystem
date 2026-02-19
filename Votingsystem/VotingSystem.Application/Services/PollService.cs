c8c44c3b78f5b2476cdd63f75a5e79efebe8dbb9
Marjan Haghighat <MarjanHD1372@gmail.com>
2026-02-19 15:28:37 +0000

    Implement logging service and register DI

--- Votingsystem/VotingSystem.Application/Services/PollService.cs
+++ Votingsystem/VotingSystem.Application/Services/PollService.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using VotingSystem.Application.Services.Logging;
 
 namespace VotingSystem.Application.Services
 {
-    internal class PollService
+    public class PollService : IPollService
     {
+        private readonly ILoggingService _logging;
+
+        public PollService(ILoggingService logging)
+        {
+            _logging = logging;
+        }
+
+        public void CreatePoll(string title)
+        {
+            _logging.LogInfo($"Poll created: {title}");
+        }
+
+        public void Vote(int pollId, int userId)
+        {
+            _logging.LogInfo($"User {userId} voted in poll {pollId}");
+        }
     }
+
 }
