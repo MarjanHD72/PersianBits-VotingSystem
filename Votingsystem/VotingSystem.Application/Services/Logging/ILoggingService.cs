bf6772b88a0b3d62ac8a63a0c7c32fbe621267df
Marjan Haghighat <MarjanHD1372@gmail.com>
2026-03-26 15:04:55 +0000

    unused files Deleted

--- Votingsystem/VotingSystem.Application/Services/Logging/ILoggingService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VotingSystem.Application.Services.Logging
-{
-    public interface ILoggingService
-    {
-        void LogInfo(string message);
-        void LogWarning(string message);
-        void LogError(string message);
-    }
-}
